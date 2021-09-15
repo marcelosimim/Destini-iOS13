5fb67ca24ebdca1cc748b448cd6400afeb744911
marcelosimim <54868944+marcelosimim@users.noreply.github.com>
2021-09-15 01:31:54 -0300

    mvc challenge

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = storyBrain.stories[0].title
+        choice1Button.setTitle(storyBrain.stories[0].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[0].choice2, for: .normal)
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let index = storyBrain.defineWay( sender.currentTitle!)
+        updateUI(index)
+    }
+    
+    func updateUI(_ index:Int){
+        storyLabel.text = storyBrain.stories[index].title
+        choice1Button.setTitle(storyBrain.stories[index].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[index].choice2, for: .normal)
+    }
 }
 
